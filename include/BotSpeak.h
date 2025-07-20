063e1ff1fb660df06825310b7135f622f52cb97c
Eccentric Orange <eccentric.orange2@gmail.com>
2025-07-20 12:43:46 +0530

    make some headers

--- include/BotSpeak.h
+++ include/BotSpeak.h
@@ -5,8 +5,6 @@
 #ifndef BOT_SPEAK_H__
 #define BOT_SPEAK_H__
 
-extern const uint8_t MAX_DATA_LENGTH;
-
 typedef struct {
 
     uint64_t packet_id;
@@ -17,7 +15,14 @@ typedef struct {
 } Packet_TypeDef;
 
 
-int assemble_packet(Packet_TypeDef* packet, uint8_t* destination_buffer, uint16_t* destination_length);
-int parse_packet(Packet_TypeDef* packet, uint8_t* source_buffer, uint16_t source_length);
+int assemble_packet(Packet_TypeDef* source_packet, uint8_t* destination_buffer, uint16_t* destination_length);
+int parse_packet(Packet_TypeDef* destination_packet, uint8_t* source_buffer, uint16_t source_length);
+
+int parse_packet_ID(Packet_TypeDef* packet);
+int parse_config_packet(Packet_TypeDef* packet);
+int parse_request_packet(Packet_TypeDef* packet);
+int parse_data_packet(Packet_TypeDef* packet);
+int parse_command_packet(Packet_TypeDef* packet);
+int parse_emergency_packet(Packet_TypeDef* packet);
 
 #endif // BOT_SPEAK_H__
--- src/BotSpeak.c
+++ src/BotSpeak.c
@@ -1,1 +1,33 @@
 #include "BotSpeak.h"
+#include <errno.h>
+
+int parse_packet_ID(Packet_TypeDef* packet) {
+
+    switch (packet->packet_id) {
+        case 0x00 ... 0x1F:
+            parse_config_packet(packet);
+            break;
+
+        case 0x20 ... 0x3F:
+            parse_request_packet(packet);
+            break;
+
+        case 0x40 ... 0x5F:
+            parse_data_packet(packet);
+            break;
+
+        case 0x60 ... 0x7F:
+            parse_command_packet(packet);
+            break;
+
+        case 0x80 ... 0x9F:
+            parse_emergency_packet(packet);
+            break;
+
+        default:
+            break;
+            return -EINVAL;
+    }
+
+    return 0;
+}
