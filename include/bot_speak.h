22638c6840dd1f8f0157888905c19daed724a7e9
Eccentric Orange <eccentric.orange2@gmail.com>
2025-07-20 23:46:32 +0530

    forgot prefix :}

--- include/bot_speak.h
+++ include/bot_speak.h
@@ -21,11 +21,11 @@ void botSpeak_deserialize(void* destinationBuffer, uint8_t *numberElements, uint
 int botSpeak_packFrame(DataFrame_TypeDef* sourceFrame, uint8_t* destinationBuffer, uint8_t* destinationLength);
 int botSpeak_unpackFrame(DataFrame_TypeDef* destinationFrame, uint8_t* sourceBuffer, uint8_t sourceLength);
 
-int parseFrameID(DataFrame_TypeDef* frame);
-extern int parseConfigFrame(DataFrame_TypeDef* frame);
-extern int parseRequestFrame(DataFrame_TypeDef* frame);
-extern int parseResponseFrame(DataFrame_TypeDef* frame);
-extern int parseCommandFrame(DataFrame_TypeDef* frame);
-extern int parseEmergencyFrame(DataFrame_TypeDef* frame);
+int botSpeak_parseFrameID(DataFrame_TypeDef* frame);
+extern int botSpeak_parseConfigFrame(DataFrame_TypeDef* frame);
+extern int botSpeak_parseRequestFrame(DataFrame_TypeDef* frame);
+extern int botSpeak_parseResponseFrame(DataFrame_TypeDef* frame);
+extern int botSpeak_parseCommandFrame(DataFrame_TypeDef* frame);
+extern int botSpeak_parseEmergencyFrame(DataFrame_TypeDef* frame);
 
 #endif // BOT_SPEAK_H__
--- src/bot_speak_task_ops.c
+++ src/bot_speak_task_ops.c
@@ -1,29 +1,29 @@
 #include <errno.h>
 #include "bot_speak.h"
 
-int parseFrameID(DataFrame_TypeDef* frame) {
+int botSpeak_parseFrameID(DataFrame_TypeDef* frame) {
     int status;
 
     switch (frame->frameID) {
 
         case 0x00 ... 0x1F:
-            status = parseConfigFrame(frame);
+            status = botSpeak_parseConfigFrame(frame);
             break;
 
         case 0x20 ... 0x3F:
-            status = parseRequestFrame(frame);
+            status = botSpeak_parseRequestFrame(frame);
             break;
 
         case 0x40 ... 0x5F:
-            status = parseResponseFrame(frame);
+            status = botSpeak_parseResponseFrame(frame);
             break;
 
         case 0x60 ... 0x7F:
-            status = parseCommandFrame(frame);
+            status = botSpeak_parseCommandFrame(frame);
             break;
 
         case 0x80 ... 0x9F:
-            status = parseEmergencyFrame(frame);
+            status = botSpeak_parseEmergencyFrame(frame);
             break;
 
         default:
